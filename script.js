875585ddf0fe6028611f93411a05214be835ea39
Blade <bladeprojectz@gmail.com>
2024-01-27 18:21:12 +0800

    Changed header in index.html to flexbox and added text also removed JS

--- script.js
+++
@@ -1,4 +0,0 @@
-
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript (figured out how to change the color :P)";
-document.querySelector("body").appendChild(h2);
